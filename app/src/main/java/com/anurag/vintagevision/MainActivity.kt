91eaad79d1a156877c52a8357ed0dd54a4c8e4f1
Ayush Chandan <ayushchandanworks@gmail.com>
2023-09-14 12:13:39 +0530

    feat: Added View Binding

--- app/src/main/java/com/anurag/vintagevision/MainActivity.kt
+++ app/src/main/java/com/anurag/vintagevision/MainActivity.kt
@@ -2,10 +2,21 @@ package com.anurag.vintagevision
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.anurag.vintagevision.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        binding.btnCamera.setOnClickListener {
+            
+        }
+        setContentView(view)
     }
+
+
 }
