0d683a47c1114e5c96f98d67cbcebacd555eddf7
AnuragDOES <anuragchandel66@gmail.com>
2023-11-23 23:29:36 +0530

    added button functionalities

--- app/src/main/java/com/anurag/vintagevision/ModelPreviewActivity.kt
+++ app/src/main/java/com/anurag/vintagevision/ModelPreviewActivity.kt
@@ -2,7 +2,10 @@ package com.anurag.vintagevision
 
 import android.content.ContentValues
 import android.content.ContentValues.TAG
+import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.Image
 import android.os.Build
 import android.os.Build.VERSION
 import androidx.appcompat.app.AppCompatActivity
@@ -12,11 +15,15 @@ import android.util.Log
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY
 import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 import com.anurag.vintagevision.databinding.ActivityModelPreviewBinding
+import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.ExecutorService
@@ -24,6 +31,8 @@ import java.util.concurrent.Executors
 
 class ModelPreviewActivity : AppCompatActivity() {
 
+    private lateinit var cameraProvider: ProcessCameraProvider
+    private lateinit var preview: Preview
     private lateinit var binding: ActivityModelPreviewBinding
     private var imageCapture: ImageCapture? = null
     private lateinit var cameraExecutor: ExecutorService
@@ -31,60 +40,65 @@ class ModelPreviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityModelPreviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        startCamera()
         binding.capturePhoto.setOnClickListener {
             takePhoto()
+
+
         }
+
         // TODO("Implement a double tap feature here")
-
-        startCamera()
-
         cameraExecutor = Executors.newSingleThreadExecutor()
 
+    }
+    private fun createBitmapFromByteBuffer(byteBuffer: ByteBuffer): Bitmap {
+        val byteArray = ByteArray(byteBuffer.remaining())
+        byteBuffer.get(byteArray)
+        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
     }
 
     private fun takePhoto() {
         // Get a stable reference of the modifiable image capture use case
         val imageCapture = imageCapture ?: return
 
-        // Create time stamped name and MediaStore entry.
-        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
-            .format(System.currentTimeMillis())
-        val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Vintage Vision")
-            }
-        }
-
-        // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions
-            .Builder(contentResolver,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues)
-            .build()
 
         // Set up image capture listener, which is triggered after photo has
         // been taken
         imageCapture.takePicture(
-            outputOptions,
             ContextCompat.getMainExecutor(this),
-            object : ImageCapture.OnImageSavedCallback {
+            object : ImageCapture.OnImageCapturedCallback() {
                 override fun onError(exc: ImageCaptureException) {
                     Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
                 }
 
-                override fun
-                        onImageSaved(output: ImageCapture.OutputFileResults){
-                    val msg = "Photo capture succeeded: ${output.savedUri}"
-                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG, msg)
-                    //Toast.makeText(this, "success")
+                override fun onCaptureSuccess(imageProxy: ImageProxy) {
+                    //imageProxy to byteBuffer
+                    val planeBuffer = imageProxy.planes[0].buffer
+                    val bytes = ByteArray(planeBuffer.remaining())
+                    planeBuffer.get(bytes)
+                    val byteBuffer = ByteBuffer.wrap(bytes)
+
+                    imageProxy.close()
+
+//                    intent to next activity
+
+//                    intent.putExtra("imageData", byteBuffer.array())
+                    Toast.makeText(baseContext,"yaha",Toast.LENGTH_LONG).show()
+//                    navigateToIVactivity()
+//                    cameraProvider.unbind(preview)
+//                    val bitmap = createBitmapFromByteBuffer(byteBuffer)
+//                    binding.ivDemo2.setImageBitmap(bitmap)
+
                 }
             }
         )
 
 
+    }
+
+    private fun navigateToIVactivity() {
+        val intent = Intent(this, ImageViewActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onDestroy() {
@@ -97,16 +111,16 @@ class ModelPreviewActivity : AppCompatActivity() {
 
         cameraProviderFuture.addListener({
             // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            cameraProvider = cameraProviderFuture.get()
 
             // Preview
-            val preview = Preview.Builder()
+            preview = Preview.Builder()
                 .build()
                 .also {
                     it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
                 }
 
-            imageCapture = ImageCapture.Builder().build()
+            imageCapture = ImageCapture.Builder().setCaptureMode(CAPTURE_MODE_MAXIMIZE_QUALITY).build()
 
             // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
@@ -122,14 +136,11 @@ class ModelPreviewActivity : AppCompatActivity() {
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
-
         }, ContextCompat.getMainExecutor(this))
-
-
     }
 
-    companion object {
-        private const val TAG = "VintageVision"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-    }
+//    companion object {
+//        private const val TAG = "VintageVision"
+//        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+//    }
 }
